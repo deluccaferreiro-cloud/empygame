37ec1b453ea0d2ae6944fb3f010d1113aa03f260
Dantelz <madeluccaferreiro@alumno.huergo.edu.ar>
2026-08-24 08:57:04 -0300

    dcsa

--- archivo.py
+++ archivo.py
@@ -5,41 +5,34 @@ ancho = 800
 alto = 600
 ventana = pygame.display.set_mode((ancho, alto))
 pygame.display.set_caption("Juego")
-
 ejecutando = True
-x = 100
-y = 200
-velocidad = 5
 reloj = pygame.time.Clock()
+y = 225
+x = 375
+velocidad = 5
+tamaño = 50
 
 while ejecutando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
-            ejecutando = False  
-    y = 200
-    
-    ventana.fill((25, 30, 40))
-    pygame.draw.rect(ventana, (80, 200,255), (x, y, 50, 50))
-    pygame.display.flip()
-
+            ejecutando = False 
     teclas = pygame.key.get_pressed()
-
+    
     if teclas[pygame.K_w]:
         y -= velocidad
 
     if teclas[pygame.K_s]:
         y += velocidad
-
+    
     if teclas[pygame.K_a]:
         x -= velocidad
-
+    
     if teclas[pygame.K_d]:
         x += velocidad
-
+    
+    ventana.fill((25, 30, 40))
+    pygame.draw.rect(ventana, (80, 200,255), (x, y, tamaño, tamaño))
     pygame.display.flip()
     reloj.tick(60)
 
-    ventana.fill((25, 30, 40))
-    
-
-
+pygame.quit()
