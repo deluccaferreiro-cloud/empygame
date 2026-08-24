cce0e3fd07bd7ab7d03ecc24da73b4fe8e38a1c1
Dantelz <madeluccaferreiro@alumno.huergo.edu.ar>
2026-08-24 08:17:56 -0300

    da

--- archivo.py
+++ archivo.py
@@ -5,3 +5,18 @@ ancho = 800
 alto = 600
 ventana = pygame.display.set_mode((ancho, alto))
 pygame.display.set_caption("Mi juego")
+
+ejecutando = True
+
+while ejecutando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            ejecutando = False
+
+    ventana.fill((0, 0, 0))
+    pygame.display.flip()
+
+x = 100
+y = 100
+
+pygame.draw.rect(ventana, (80, 200,255), (x, y, 50, 50))
